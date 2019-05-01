332ccd601536ef985a9ce4ff209b6c66c6c7cd93
Ugurcan Lacin <ugurcanlacin@gmail.com>
2019-05-01 17:23:03 +0200

    initial commit

---
+++ src/main/java/com/myboilerplate/myboilerplate/MyBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.myboilerplate.myboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myboilerplate/myboilerplate/MyBoilerplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myboilerplate.myboilerplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBoilerplateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
